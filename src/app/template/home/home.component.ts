fd35880dd722a7d9e7ddb6dc9971b5d37099a23b
Your Name <eduardokaue277@gmail.com>
2024-04-11 22:35:11 -0300

    Feat: Update de card e interpolações

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { __setFunctionName } from 'tslib';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,8 @@ export class HomeComponent {
   anuncio = `o ${this.nomeProduto} Está em promoção `;
   idProduto : number = 123;
   precoProduto : number = 2.59;
-  promocao : boolean = true;
+  promocao : boolean = false;
+  foto : string = 'assets/img/crud.png'
 
 
   constructor() {
@@ -29,19 +31,115 @@ export class HomeComponent {
     var idade = 10
     console.log(`Minha idade é: `, idade);
 
+    /*
     function imprimeidade() {
       var idade = 50
       console.log(`Minha idade é: `, idade)
     }
-     imprimeidadefuc()
-
-     fuction imprimeidade({
-      for (let idade = 30; idade <= 40; idade++){
+     imprimeidade();
+    */
+    /*
+     function imprimirIdade()
+     {
+      for (let idade = 30; idade <= 40; idade++)
+      {
         console.log(`idade dentro do for: `, idade)
       }
       console.log(`idade fora do for: ,` , idade)
      }
-     imprimiridade)
+
+     imprimirIdade();
+     */
+
+      /*
+      var variavelN1 =0;
+      var variavelN1 = 10;
+
+      console.log('variavel é', variavelN1);
+      function funcao()
+      {
+
+        var outra_var = 20;
+        var outra_var = 30;
+
+      console.log('outra variavel', outra_var)
+
+      }
+
+      funcao();
+
+      /*
+      var a = 10;
+      let b = 20;
+
+    function bar()
+    {
+        var a = 30;
+        let b = 40;
+
+        if(true)
+        {
+          var a = 50;
+          let b = 60;
+
+          console.log('Variavel a dentro do IF', a);
+          console.log("Variavel b dentro do if", b);
+
+        }
+
+        console.log('Variavel a dentro do Funação', a);
+        console.log("Variavel b dentro do Função", b);
+    }
+
+    bar();
+    console.log("Variavel fora da função", a)
+    console.log('Variavel b fora da função', b)
+*/
+
+
+/*
+let a = 1;
+
+    function bar()
+      {
+        console.log('Vqariavel a:', a);
+        let b = 2
+
+        if(true)
+        {
+          let c = 3;
+          let a = 4;
+          console.log("Variavel a dentro do if:", a)
+          console.log("Variavel b dentro do if:", b)
+          console.log("Variavel c dentro do if:", c)
+        }
+
+        console.log("Variavel a fora do if:", a)
+        console.log("Variavel b fora do if:", b)
+        //console.log(c);
+
+      }
+
+      bar();
+*/
+/*
+      var a= 5;
+      var b =10;
+
+      if(a = 5)
+      {
+          let a = 4;
+          let b= 1;
+
+          console.log('Variavel a dentro de if :', a);
+          console.log('variavel b fora do if:', b);
+
+      }
+
+      console.log('Variavel a fora do if:', a );
+      console.log('Variavel b fora do if:', b)
+
+*/
 
 
 
