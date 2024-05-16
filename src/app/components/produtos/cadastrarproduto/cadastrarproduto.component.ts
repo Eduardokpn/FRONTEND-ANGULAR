1a181fc1e92290d198f9bd32a51f5ee180cbcac4
Your Name <EDUARDOKAUE277@GMAIL.COM>
2024-05-16 20:12:38 -0300

    UPDATE TWO WAY BINDING

--- src/app/components/produtos/cadastrarproduto/cadastrarproduto.component.ts
+++ src/app/components/produtos/cadastrarproduto/cadastrarproduto.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './cadastrarproduto.component.html',
   styleUrls: ['./cadastrarproduto.component.css']
 })
-export class CadastrarprodutoComponent {
+export class CadastrarprodutoComponent
+{
+    nome: string = '';
+    validade: string = "";
+    preco : number = 0;
+    p: any
+    ngOnInit(): void{}
+
+    salvarProduto(): void
+     {
+
+        console.log('nome: ', this.nome);
+        console.log('Validade: ', this.validade);
+        console.log('preco: ' ,this.preco);
+        alert('Salvo com sucesso');
+
+     }
+
 
 }
